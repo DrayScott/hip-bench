a3b1448fd07adb385601565987e0048ac498b11e
Tester <tester@3c18-madrid-sut08.amd.com>
2025-06-17 13:35:46 -0400

    First Working Benchmark: vector add

---
+++ build/CMakeFiles/hip_runtime_bench.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "HIP"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_HIP
+  "/home/tester/dev/hip-bench/kernels/vector_add.hip" "/home/tester/dev/hip-bench/build/CMakeFiles/hip_runtime_bench.dir/kernels/vector_add.hip.o"
+  )
+set(CMAKE_HIP_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_HIP
+  "__HIP_PLATFORM_AMD__"
+  "__HIP_ROCclr__=1"
+  )
+
+# The include file search paths:
+set(CMAKE_HIP_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tester/dev/hip-bench/src/main.cpp" "CMakeFiles/hip_runtime_bench.dir/src/main.cpp.o" "gcc" "CMakeFiles/hip_runtime_bench.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
